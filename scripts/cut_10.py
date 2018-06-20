97c32c2525760dfccd4b8e27c2a637d27638ea12
dmatelska <dmatelska@genesilico.pl>
2018-06-20 15:37:50 +0200

    Trimming barcodes from R2

--- scripts/cut_10.py
+++ scripts/cut_10.py
@@ -1,6 +1,8 @@
 from __future__ import print_function
 import sys
 
+LAST = False
+
 l = 10
 infile, outfile = sys.argv[1], sys.argv[2]
 with open(outfile, 'w') as f:
@@ -8,7 +10,10 @@ with open(outfile, 'w') as f:
 		if line[:1] == '>':
 			print(line.strip(), file=f)
 		else:
-			seq = line.strip()[:10]
+			if LAST:
+				seq = line.strip()[-10:][::-1]
+			else:
+				seq = line.strip()[:10]
 			n = len(seq)
 			if n < l:
 				seq = seq + ''.join((l-n) * ['-'])
